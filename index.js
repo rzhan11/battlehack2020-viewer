a55138a8d8e52a104a1ecc02e40fad52a97814e9
Richard Zhan <richymanzhan@gmail.com>
2020-04-20 16:53:21 -0400

    added more qol features

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/nodejs
 
-
 // -------------- load packages -------------- //
 var express = require('express')
 var path = require('path');
@@ -9,13 +8,11 @@ var fs = require('fs');
 
 var app = express();
 
-
 // -------------- express initialization -------------- //
 app.set('port', process.env.PORT || 8080 );
 
 // tell express that the view engine is hbs
 app.set('view engine', 'hbs');
-
 
 // -------------- express endpoint definition -------------- //
 
@@ -24,31 +21,6 @@ app.use('/css', express.static(path.join(__dirname, 'css')));
 app.get('/', function(req, res){
     console.log("home anon()");
     res.render("home", {
-        "session": req.session,
-    });
-});
-
-app.get('/viewer', function(req, res){
-    console.log("viewer anon()");
-
-    res.render("viewer", {
-
-    });
-});
-
-app.get('/', function(req, res){
-    console.log("pathing_visualizer anon()");
-
-    res.render("home", {
-    });
-
-});
-
-// WILDCARD HANDLERS MUST COME AFTER ALL OTHER EXPLICIT ENDPOINTS
-app.get('/:stuff', function(req, res){
-    res.render("notfound", {
-        "session": req.session,
-        "url": req.params.stuff
     });
 });
 
